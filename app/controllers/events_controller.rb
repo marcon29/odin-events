755e1c94d769dffdf1922e566570b841eac89cfb
marcon29 <online@markstabler.com>
2022-02-28 17:22:42 -0700

    generated resource for Event, built out basic CRUD

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,52 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    
+    
+    @event.assign_attributes(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    redirect_to events_path
+  end
+
+
+  private
+  def event_params
+    params.require(:event).permit(:id, :name, :date, :time)
+  end
+
+end
